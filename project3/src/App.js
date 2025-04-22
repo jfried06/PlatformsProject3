0495ab775b670a6f995873f740e67e7f8d31c1bf
“renvelente” <clarencevpot@gmail.com>
2025-04-22 10:55:16 -0400

    added title, date, and explanation

--- project3/src/App.js
+++ project3/src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import { useState, useEffect } from "react";
 
 function App() {
-
+  const [title, setTitle] = useState();
+  const [date, setDate] = useState();
   const [imageURL, setImageURL] = useState();
+  const [explanation, setExplanation] = useState();
+
 
   function handlePictureClick() {
     // API KEY: jVPtZqCOqGMYnklNnjIUnygvyObM2G9ymzmKh303
@@ -14,7 +17,10 @@ function App() {
     fetch('https://api.nasa.gov/planetary/apod?api_key=jVPtZqCOqGMYnklNnjIUnygvyObM2G9ymzmKh303')
       .then(response => response.json())
       .then(data => {
+        setTitle(data.title);
+        setDate(data.date);
         setImageURL(data.url);
+        setExplanation(data.explanation);
       })
       .catch(error => console.error(error));
   }, []);
@@ -22,7 +28,10 @@ function App() {
   return (
     <div>
       <RandomButton onClick={handlePictureClick} />
+      <h1>{title}</h1>
+      <h2>{date}</h2>
       <img src={imageURL} alt="spaceImage" />
+      <p>{explanation}</p>
     </div>
   );
 }
