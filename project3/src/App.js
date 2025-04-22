d3500674821465c8c1cd5be1c07ea5a8e18e2379
“renvelente” <clarencevpot@gmail.com>
2025-04-22 10:44:41 -0400

    today's picture gets displayed

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,35 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import { useState, useEffect } from "react";
 
 function App() {
 
+  const [imageURL, setImageURL] = useState();
+
   function handlePictureClick() {
+    // API KEY: jVPtZqCOqGMYnklNnjIUnygvyObM2G9ymzmKh303
     // use API to get new picture
   }
 
+  useEffect(() => {
+    fetch('https://api.nasa.gov/planetary/apod?api_key=jVPtZqCOqGMYnklNnjIUnygvyObM2G9ymzmKh303')
+      .then(response => response.json())
+      .then(data => {
+        setImageURL(data.url);
+      })
+      .catch(error => console.error(error));
+  }, []);
+
   return (
-    <RandomButton onClick={handlePictureClick} />
+    <div>
+      <RandomButton onClick={handlePictureClick} />
+      <img src={imageURL} alt="spaceImage" />
+    </div>
   );
 }
 
